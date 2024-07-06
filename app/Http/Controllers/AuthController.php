9352113b90d08563a3b2b76e1abab6a11bec230d
George Chukwuebuka <ebuka.g@zyonel.com>
2024-07-06 09:27:10 +0100

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,14 +16,12 @@ class AuthController extends Controller
         $visitor = request()->query('visitor_name');
         $location = Location::get();
 
-        dd($location);
-
         $weatherClient = new Weather();
         $weather = $weatherClient->getCurrentByCity($location->cityName);
 
         $visitor_name = ($visitor) ? $visitor : 'Guest';    
         
-        // $response['client_ip'] =  $ip;
+        $response['client_ip'] =  $location->ip;
         $response['location'] = $location->cityName ?? $location->countryName;
         $response['salutation'] =' Hello, '. trim($visitor_name, "\"' \t\n\r\0\x0B") . '!, the temperature is '. $weather->main->temp . ' degrees Celcius in ' .  $location->cityName;
         return response()->json($response, 200); 
