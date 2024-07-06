c0514ae3efcb59ae1368805511a10b8f6e0af213
George Chukwuebuka <ebuka.g@zyonel.com>
2024-07-06 06:59:38 +0100

    update

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use RakibDevs\Weather\Weather;
+use Stevebauman\Location\Facades\Location;
+
+
+
+class AuthController extends Controller
+{
+    
+    public function welcome(Request $request)
+    {
+        $ip = request()->ip();
+        $visitor = request()->query('visitor_name');
+        
+        // $location = Location::get('105.48.250.156');
+        $location = Location::get($ip);
+
+        
+        $weatherClient = new Weather();
+        $weather = $weatherClient->getCurrentByCity($location->cityName);
+
+        $visitor_name = ($visitor) ? $visitor : 'Guest';    
+        
+        $response['client_ip'] =  $ip;
+        $response['location'] = $location->cityName ?? $location->countryName;
+        $response['salutation'] =' Hello, '. trim($visitor_name, "\"' \t\n\r\0\x0B") . '!, the temperature is '. $weather->main->temp . ' degrees Celcius in ' .  $location->cityName;
+        return response()->json($response, 200); 
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
-use RakibDevs\Weather\Weather;
 use Illuminate\Support\Facades\Route;
-use Stevebauman\Location\Facades\Location;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,22 +25,4 @@ Route::get('/home', function () {
 
 
 
-Route::get('/api/hello', function (Request $request) {
-
-    $ip = request()->ip();
-    $visitor = request()->query('visitor_name');
-    
-    // $location = Location::get('105.48.250.156');
-    $location = Location::get($ip);
-
-    
-    $weatherClient = new Weather();
-    $weather = $weatherClient->getCurrentByCity($location->cityName);
-
-    $visitor_name = ($visitor) ? $visitor : 'Guest';    
-    
-    $response['client_ip'] =  $ip;
-    $response['location'] = $location->cityName ?? $location->countryName;
-    $response['salutation'] =' Hello, '. trim($visitor_name, "\"' \t\n\r\0\x0B") . '!, the temperature is '. $weather->main->temp . ' degrees Celcius in ' .  $location->cityName;
-    return response()->json($response, 200); 
-});
+Route::get('/api/hello',  [ AuthController::class, 'welcome'] );
